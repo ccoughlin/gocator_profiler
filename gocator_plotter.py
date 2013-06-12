9d1b5f632ef75e42c03ca64e55e82110d474de32
Chris Coughlin <chriscoughlin@gmail.com>
2013-06-12 13:55:56 -0500

    Added drag&drop to gocator_plotter

--- gocator_plotter.py
+++ gocator_plotter.py
@@ -17,12 +17,23 @@ from matplotlib.backends.backend_wxagg import NavigationToolbar2Wx
 import numpy as np
 import scipy.signal
 import os.path
+import sys
+import fileinput
 
 widget_margin = 3 # Default margin around widgets
 ctrl_pct = 1.0 # Default to 100% resizing factor for controls
 lbl_pct = 0.25 # Default to 25% resizing factor for labels
 sizer_flags = wx.ALL | wx.EXPAND # Default resizing flags for controls
 lblsizer_flags = wx.ALIGN_CENTRE_VERTICAL | wx.ALL # Default resizing flags for labels
+
+class FileDropTarget(wx.FileDropTarget):
+
+    def __init__(self, window):
+        super(FileDropTarget, self).__init__()
+        self.window = window
+
+    def OnDropFiles(self, x, y, filenames):
+        self.window.plot_data(filenames[0])
 
 class FloatRangeDialog(wx.Dialog):
     """Dialog to specify a floating-point numeric range.  Defaults to allowing any
@@ -80,12 +91,15 @@ class UI(wx.Frame):
         wx.Frame.__init__(self, parent=None, title=self.base_title, *args, **kwargs)
         self.init_ui()
         self.data_fname = None
+        self.point_color = (0.31, 0.40, 0.51)
 
     def init_ui(self):
         """Creates the user interface"""
         self.init_menu()
         self.SetSize(wx.Size(640, 480))
         self.main_panel = wx.Panel(self)
+        file_drop_target = FileDropTarget(self)
+        self.main_panel.SetDropTarget(file_drop_target)
         self.main_panel_sizer = wx.BoxSizer(wx.VERTICAL)
         self.figure = Figure()
         self.canvas = FigureCanvas(self.main_panel, wx.ID_ANY, self.figure)
@@ -110,6 +124,12 @@ class UI(wx.Frame):
                                          help="Reads and plots an XYZ CSV data file")
         self.Bind(wx.EVT_MENU, self.on_plot_data, id=self.plotdata_mnui.GetId())
         self.file_mnu.AppendItem(self.plotdata_mnui)
+        self.quit_mnui = wx.MenuItem(self.file_mnu, wx.ID_ANY, text="Exit Plotter\tCTRL+Q")
+        self.Bind(wx.EVT_MENU, self.on_quit, id=self.quit_mnui.GetId())
+        self.file_mnu.AppendItem(self.quit_mnui)
+        self.menubar.Append(self.file_mnu, "File")
+        
+        self.plot_mnu = wx.Menu()
         plottype_mnu = wx.Menu()
         self.plot_pointcloud_mnui = wx.MenuItem(plottype_mnu, wx.ID_ANY, text="Point Cloud",
                                                 help="Plots the laser profile as a point cloud", kind=wx.ITEM_RADIO)
@@ -124,19 +144,22 @@ class UI(wx.Frame):
                                                 kind=wx.ITEM_RADIO)
         plottype_mnu.AppendItem(self.plot_wireframe_mnui)
         self.Bind(wx.EVT_MENU, self.on_plot_type, id=self.plot_wireframe_mnui.GetId())
-        self.file_mnu.AppendSubMenu(plottype_mnu, "Type Of Plot")
-        self.plotrawdata = wx.MenuItem(self.file_mnu, wx.ID_ANY, text="Plot Raw Data",
+        self.plot_mnu.AppendSubMenu(plottype_mnu, "Type Of Plot")
+        
+        self.plot_color_mnui = wx.MenuItem(self.plot_mnu, wx.ID_ANY, text="Choose Point Color", help="Select the point color to use in point cloud plots")
+        self.Bind(wx.EVT_MENU, self.on_choosepointcolor, id=self.plot_color_mnui.GetId())
+        self.plot_mnu.AppendItem(self.plot_color_mnui)
+        
+        self.plotrawdata = wx.MenuItem(self.plot_mnu, wx.ID_ANY, text="Plot Raw Data",
                                            help="Plot raw vs. filtered data", kind=wx.ITEM_CHECK)
         self.Bind(wx.EVT_MENU, self.on_filter_data, id=self.plotrawdata.GetId())
-        self.file_mnu.AppendItem(self.plotrawdata)
-        self.setzlim_mnui = wx.MenuItem(self.file_mnu, wx.ID_ANY, text="Set Z Axis Limits...\tCTRL+Z",
+        self.plot_mnu.AppendItem(self.plotrawdata)
+        self.setzlim_mnui = wx.MenuItem(self.plot_mnu, wx.ID_ANY, text="Set Z Axis Limits...\tCTRL+Z",
                                         help="Sets the limits of the Z Axis")
         self.Bind(wx.EVT_MENU, self.on_setzlim, id=self.setzlim_mnui.GetId())
-        self.file_mnu.AppendItem(self.setzlim_mnui)
-        self.quit_mnui = wx.MenuItem(self.file_mnu, wx.ID_ANY, text="Exit Plotter\tCTRL+Q")
-        self.Bind(wx.EVT_MENU, self.on_quit, id=self.quit_mnui.GetId())
-        self.file_mnu.AppendItem(self.quit_mnui)
-        self.menubar.Append(self.file_mnu, "Operation")
+        self.plot_mnu.AppendItem(self.setzlim_mnui)
+        
+        self.menubar.Append(self.plot_mnu, "Plot")
         self.SetMenuBar(self.menubar)
 
     def on_plot_data(self, evt):
@@ -146,12 +169,15 @@ class UI(wx.Frame):
         wildcards = "|".join([txt_wildcards, allfiles_wildcard])
         file_dlg = wx.FileDialog(parent=self, message="Please specify a file", wildcard=wildcards, style=wx.FD_OPEN)
         if file_dlg.ShowModal() == wx.ID_OK:
-            self.data_fname = file_dlg.GetPath()
-            self.plot_data(self.data_fname)
-            self.SetTitle(' - '.join([self.base_title, os.path.basename(self.data_fname)]))
+            #self.data_fname = file_dlg.GetPath()
+            #self.plot_data(self.data_fname)
+            #self.SetTitle(' - '.join([self.base_title, os.path.basename(self.data_fname)]))
+            self.plot_data(file_dlg.GetPath())
 
     def plot_data(self, data_fname):
         """Reads and plots the specified data file"""
+        self.data_fname = data_fname
+        self.SetTitle(' - '.join([self.base_title, os.path.basename(self.data_fname)]))
         self.figure.clf()
         self.axes = self.figure.add_subplot(111, projection='3d')
         self.axes.grid(True)
@@ -160,7 +186,7 @@ class UI(wx.Frame):
             x, y, z = self.get_data(data_fname)
             if self.plot_pointcloud_mnui.IsChecked():
                 # Plot a point cloud
-                self.axes.plot(x, y, z, c='#4F6581', linestyle='', marker=',', rasterized=True)
+                self.axes.plot(x, y, z, c=self.point_color, linestyle='', marker=',', rasterized=True)
             else:
                 xi = np.linspace(min(x), max(x), num=100)
                 yi = np.linspace(min(y), max(y), num=100)
@@ -176,7 +202,7 @@ class UI(wx.Frame):
                     # Interpolate a wireframe surface from the point cloud to plot
                     # Uses a higher resolution presentation than the surface - surface tends to bog down
                     # on some systems with stride set to 1
-                    surf = self.axes.plot_wireframe(X, Y, Z, rstride=1, cstride=1, color='#4F6581',
+                    surf = self.axes.plot_wireframe(X, Y, Z, rstride=1, cstride=1, color=self.point_color,
                                               linewidth=1, antialiased=True)
                 self.axes.set_zlim3d(np.min(Z), np.max(Z))
             self.axes.set_xlabel('X Position [mm]')
@@ -262,16 +288,34 @@ class UI(wx.Frame):
     def refresh_plot(self):
         """Forces plot to redraw itself"""
         self.canvas.draw()
+        
+    def on_choosepointcolor(self, evt):
+        """Handles request to change plot point color"""
+        color_dlg = wx.ColourDialog(self)
+        color_dlg.GetColourData().SetChooseFull(True)
+        if color_dlg.ShowModal() == wx.ID_OK:
+            color_data = color_dlg.GetColourData()
+            self.point_color = [component/255. for component in color_data.GetColour().Get()]
+            if not self.plot_surface_mnui.IsChecked():
+                #self.re
+                self.plot_data(self.data_fname)
 
     def on_quit(self, evt):
         """Handles request to exit the program"""
         self.Destroy()
 
-def main():
+def main(fname=None):
     app = wx.PySimpleApp()
     ui = UI()
     ui.Show(True)
+    if fname is not None:
+        ui.plot_data(fname)
     app.MainLoop()
 
 if __name__ == "__main__":
-    main()
+    args = sys.argv[1:]
+    if len(args) > 0:
+        fname = args[0]
+    else:
+        fname = None
+    main(fname)
